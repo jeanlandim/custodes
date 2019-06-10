8b5c266e90649f01242f9e520cdf8282b6a44957
Jean Landim <jewanbb@gmail.com>
2019-06-10 13:14:11 -0300

    Update campos.py

--- custodes-app/modulos/campos.py
+++ custodes-app/modulos/campos.py
@@ -5,7 +5,7 @@ def Captura(arquivo):
    # Selecione o campo da primeira ocorrência até a última, e os extraia.
    campos = ["(?=Solicitação:|Incidente:).+?(?=Em)", "(?=Status:).+?(?=Ativo:)", "(?=Usuário afetado:).+?(?=,)",
            "(?=Responsável:).+?(?=,)", "(?=Grupo atribuído:).+?(?=Nível)","(?=Área do incidente:|Área da solicitação:).+?(?=Item)", 
-        "(?=Item de configuração).+?(?=ChargeBack)", "(?=Descrição).+?(?=Interrupção|Histórico)",
+        "(?=Item de configuração:).+?(?=ChargeBack)", "(?=Descrição:).+?(?=Interrupção|Histórico)",
         "(?=Script).+?(?=Pai)", "(?=Encerrar).+?(?= pm | am |Pai|System_AHD_generated,)" ]
    # Armazena todos os campos coletados para formatação do chamado (em forma de lista)
    info_chamados = []
