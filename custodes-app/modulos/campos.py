1e4ed5f55c91157ae399c5904b7ebe7649d89999
Jean Landim <jeanlandim@github.com>
2019-06-10 23:22:20 -0300

    Refinando o código para captura dos dados do chamado

--- custodes-app/modulos/campos.py
+++ custodes-app/modulos/campos.py
@@ -1,31 +1,45 @@
-# custodes - app / modulos / campos.py
+# custodes-app/modulos/campos.py
+# Importa a biblioteca de regex
 import re
+# Para debug do modulo
+def debug(_debug):
+    debug = open('debug.txt','w')
+    debug.write(_debug)
+    debug.close()
 # Define as regras para pegar a combinação de palavras para pegar o texto correto
-def Captura(arquivo):
+def Captura(Relatorio):
     # Selecione o campo da primeira ocorrência até a última, e os extraia.
-    campos = ["(?=Solicitação:|Incidente:).+?(?=Em)",
-        "(?=Status:).+?(?=Ativo:)",
-        "(?=Usuário afetado:).+?(?=,)",
-        "(?=Responsável:).+?(?=,)",
-        "(?=Grupo atribuído:).+?(?=Nível)",
-        "(?=Área do incidente:|Área da solicitação:).+?(?=Item)",
-        "(?=Item de configuração:).+?(?=ChargeBack)",
-        "(?=Descrição:).+?(?=Interrupção|Histórico)",
-        "(?=Script).+?(?=^$)",
-        "(?=Encerrar).+?(?=Pai:)"
-         ]
-    # Armazena todos os campos coletados para formatação do chamado(em forma de lista)
-    info_chamados = []
-    chamados = []
-    # Captura
-    for campo in campos:
-        regex = re.compile(campo, re.MULTILINE + re.DOTALL)
-        info_chamados.append(regex.findall(var))
-	
-    a = 0
-    for i in info_chamados[0]:
-        chamados.append(i + info_chamados[1][a] + info_chamados[2][a] + " " + info_chamados[3][a] + " " +
-                info_chamados[4][a] + info_chamados[5][a] + info_chamados[6][a] + info_chamados[7][a] +
-                info_chamados[8][a] + info_chamados[9][a])
-	a += 1
-    return (chamados)
+    # Esses são os campos que são imutáveis, ou seja, todos os chamados os têm.
+    campos_padroes = ["(?=Solicitação:|Incidente:).+?(?=Em)",
+                     "(?=Status:).+?(?=Ativo:)",
+                     "(?=Usuário afetado:).+?(?=,)",
+                     "(?=Responsável:).+?(?=,)",
+                     "(?=Grupo atribuído:).+?(?=Nível)",
+                     "(?=Área do incidente:|Área da solicitação:).+?(?=Item)",
+                     "(?=Item de configuração:).+?(?=ChargeBack)",
+                     "(?=Descrição:).+?(?=Interrupção|Histórico)"]
+    # Esses são os campos opcionais, pois alguns chamados não tem scripts e alguns, não são encerrados (os chamados "Não resolvido, fechado")
+    campos_opcionais = ["(?=Script).+?(?=^$)",
+                        "(?=Encerrar).+?(?=Pai:)",
+                        "(?:Registrar comentário).+?(?=Não resolvido, fechado)"]
+
+    # Pega o total de chamados para criação dos dicionários, os quais conteram os atributos de cada chamado   
+    total_de_chamados = len(re.findall(campos_padroes[0],Relatorio,re.MULTILINE+re.DOTALL))
+
+    # Cria o dicionário 'chamado', para guardar nas chaves (que serão representadas por números) e irão conter as informações
+    # do chamado.
+    chamado = {}
+    for chave in range(total_de_chamados):
+        chamado[chave] = [] # Cria lista para cada chamados
+    
+    # Passa para 'campo' a expressão regular para captura dos campos (padrões) 
+    for campo in campos_padroes:
+        # Compila a expressão regular que define o campo
+        exp_reg = re.compile(campo,re.MULTILINE+re.DOTALL)
+        iteracao = 0
+        for _campo in exp_reg.findall(Relatorio): # Coloca em cada chamado o campo capturado
+            chave = iteracao
+            chamado[chave].append(_campo) 
+            iteracao+=1 
+    
+    debug(str(chamado)) 
