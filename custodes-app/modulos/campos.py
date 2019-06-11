327185f76751f276c27b73aa285dfb25eee0b950
Jean Landim <jeanlandim@github.com>
2019-06-11 21:46:54 +0000

    Para continuar em casa

--- custodes-app/modulos/campos.py
+++ custodes-app/modulos/campos.py
@@ -17,11 +17,12 @@ def Captura(Relatorio):
                      "(?=Grupo atribuído:).+?(?=Nível)",
                      "(?=Área do incidente:|Área da solicitação:).+?(?=Item)",
                      "(?=Item de configuração:).+?(?=ChargeBack)",
-                     "(?=Descrição:).+?(?=Interrupção|Histórico)"]
-    # Esses são os campos opcionais, pois alguns chamados não tem scripts e alguns, não são encerrados (os chamados "Não resolvido, fechado")
-    campos_opcionais = ["(?=Script).+?(?=^$)",
-                        "(?=Encerrar).+?(?=Pai:)",
-                        "(?:Registrar comentário).+?(?=Não resolvido, fechado)"]
+                     "(?=Descrição:).+?(?=Interrupção|Histórico)",
+                     "(?=Script).+?(?=^$)",
+                     "(?=Encerrar).+?(?=Pai:)",
+                     "(?:Registrar comentário).+?(?=Não resolvido, fechado)"]
+    # Delimitador de chamado, que será usada para fazer a fatia do chamado
+    delimitador_chamado = ["(?=Solicitação:|Incidente:).+?(?=SLA)"]
 
     # Pega o total de chamados para criação dos dicionários, os quais conteram os atributos de cada chamado   
     total_de_chamados = len(re.findall(campos_padroes[0],Relatorio,re.MULTILINE+re.DOTALL))
@@ -31,15 +32,18 @@ def Captura(Relatorio):
     chamado = {}
     for chave in range(total_de_chamados):
         chamado[chave] = [] # Cria lista para cada chamados
-    
-    # Passa para 'campo' a expressão regular para captura dos campos (padrões) 
-    for campo in campos_padroes:
-        # Compila a expressão regular que define o campo
-        exp_reg = re.compile(campo,re.MULTILINE+re.DOTALL)
-        iteracao = 0
-        for _campo in exp_reg.findall(Relatorio): # Coloca em cada chamado o campo capturado
-            chave = iteracao
-            chamado[chave].append(_campo) 
-            iteracao+=1 
+    # Cria lista para guardar os chamados fatiados
+    chamados_fatiados = []
+    # Fatia os chamados
+    for iteracao in range(total_de_chamados):
+        chamados_fatiados.append(re.findall(delimitador_chamado,Relatorio,re.MULTILINE+re.DOTALL))
+        # Passa para 'campo' a expressão regular para captura dos campos (padrões) 
+        for campo in campos_padroes:
+            # Compila a expressão regular que define o campo
+            exp_reg = re.compile(campo,re.MULTILINE+re.DOTALL)
+            for _campo in exp_reg.findall(chamados_fatiados[iteracao]): # Coloca em cada chamado o campo capturado
+                chave = iteracao
+                chamado[chave].append(_campo) 
+        iteracao+=1 
     
     debug(str(chamado)) 
