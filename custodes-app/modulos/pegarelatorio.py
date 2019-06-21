50bbdedb28bb21f6fef5b9f8b455f7c02aba4a2a
Jean Landim <jewanbb@gmail.com>
2019-06-21 17:05:28 -0300

    Update pegarelatorio.py

--- custodes-app/modulos/pegarelatorio.py
+++ custodes-app/modulos/pegarelatorio.py
@@ -8,8 +8,3 @@ class PegaRelatorio(forms.Form):
     widget = forms.Textarea(
         attrs = {'placeholder': 'Cole aqui o relatório gerado pela CA'}),label='')
             # placeholder: Coloca o texto dentro textarea do formulário
-    def limpa(self):
-        dados_limpados = super(relatorio, self).clean()
-        relatorio = dados_limpados('relatorio')
-        if not relatorio:
-            raise forms.ValidationError("É necessário que você cole o relatório gerado.")
