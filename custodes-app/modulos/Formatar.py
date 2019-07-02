947309530ad2e72da7d376987ae0c3747810f9c7
Jean Landim <jeanlandim@github.com>
2019-07-02 15:20:04 -0400

    Melhorada a formatação dos módulos

--- custodes-app/modulos/Formatar.py
+++ custodes-app/modulos/Formatar.py
@@ -10,42 +10,39 @@ class Formatar():
 
         ''' Formata o relatório (já tratado) em HTML ou JSON. (No futuro poderá haver outras opçções '''    
       def Formatacao(RelatorioCru):
-            DadosLimpos = PreFormatacao(RelatorioCru)
-            NovosDadosEmJSON = {}
+          DadosLimpos = PreFormatacao(RelatorioCru)
+          NovosDadosEmJSON = {}
 
-            ''' Pré formate o relatório tirando caracters indesejados. '''
-            def PreFormatacao(RelatorioCru):
-                DadosLimpos = []
-                CaracteresIndesejados = self.CaracteresIndesejados
-                Substituir = self.Substituir
-                Substitutos = self.Substitutos
+          ''' Pré formate o relatório tirando caracters indesejados. '''
+          def PreFormatacao(RelatorioCru):
+              DadosLimpos = []
            
-                for TextoCru in RelatorioCru:
-                    TextoCru = str(TextoCru) 
-                    for Caracter in CaracteresIndesejados:
-                        TextoCru = TextoCru.replace(Caracter,"")
-                    for Indice in range(len(Substituir)): 
-                        TextoCru = TextoCru.replace(Substituir[Indice],Substitutos[Indice])
-                    DadosLimpos.append(TextoCru)
-                    return(DadosLimpos)
-     
-            def HTML(self):
+              for TextoCru in RelatorioCru:
+                  TextoCru = str(TextoCru) 
+                  for Caracter in self.CaracteresIndesejados:
+                      TextoCru = TextoCru.replace(Caracter,"")
+                  for Indice in range(len(self.Substituir)): 
+                      TextoCru = TextoCru.replace(self.Substituir[Indice],self.Substitutos[Indice])
+                  DadosLimpos.append(TextoCru)
+                  return(DadosLimpos)
+            
+           def HTML(self):
             ''' Pega o relatório e o formate em HTML. '''
-                NovosDadosEmHTML = []
-                for Dados in DadosLimpos:
-                    Dados = str(Dados)
-                    ''' Gera o relatório em HTML ''' 
-                    if Tipo is Tipos[0]:
-                       for Campos in self.CamposEmNegrito: ''' transforma tais campos em negrito, utilizando o tag <b> '''
-                           if Dados.split(":")[0] == Campos:
-                              Dados = Dados.replace(Campos,"<b>"+Campos+"</b>") 
+               NovosDadosEmHTML = []
+               for Dados in DadosLimpos:
+                   Dados = str(Dados)
+                   ''' Gera o relatório em HTML ''' 
+                   if Tipo is Tipos[0]:
+                      for Campos in self.CamposEmNegrito: ''' transforma tais campos em negrito, utilizando o tag <b> '''
+                          if Dados.split(":")[0] == Campos:
+                             Dados = Dados.replace(Campos,"<b>"+Campos+"</b>") 
 
-                           if Dados.split(":")[0] == "Incidente" or Dados.split(":")[0] == "Solicitação": '''  transforma em hyperlink o campo referente ao chamado '''
-                              NumeroDoChamado = Dados.split(":")[1]
-                              NumeroDoChamado = NumeroDoChamado.replace(" ","")
-                              Dados = "<a href="+UrlDoServiceDesk+NumeroDoChamado+" target=\"_blank\">"+Dados+"</a>" 
+                          if Dados.split(":")[0] == "Incidente" or Dados.split(":")[0] == "Solicitação": '''  transforma em hyperlink o campo referente ao chamado '''
+                             NumeroDoChamado = Dados.split(":")[1]
+                             NumeroDoChamado = NumeroDoChamado.replace(" ","")
+                             Dados = "<a href="+UrlDoServiceDesk+NumeroDoChamado+" target=\"_blank\">"+Dados+"</a>" 
 
-                              NovosDadosEmHTML.append(Dados)
-                     return(NovosDadosEmHTML)
+                             NovosDadosEmHTML.append(Dados)
+                    return(NovosDadosEmHTML)
             def JSON(self):
             ''' 
