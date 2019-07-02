c99a3597cf9901d3e966e2b7f6f554b6e5a3f0d5
Jean Landim <jeanlandim@github.com>
2019-07-02 18:29:30 -0400

    Para terminar em casa

--- custodes-app/modulos/Formatar.py
+++ custodes-app/modulos/Formatar.py
@@ -10,11 +10,10 @@ class Formatar():
 
         ''' Formata o relatório (já tratado) em HTML ou JSON. (No futuro poderá haver outras opçções '''    
       def Formatacao(RelatorioCru):
-          DadosLimpos = PreFormatacao(RelatorioCru)
-          NovosDadosEmJSON = {}
-
-          ''' Pré formate o relatório tirando caracters indesejados. '''
-          def PreFormatacao(RelatorioCru):
+          DadosFormatados = None
+          ''' Pré formate o relatório tirando caracteres indesejados. Está função é um decorator que será utilizado para 
+              para pré-formatar e o passos dados pré-formatados para função que irá gerar determinado formato (HTMl ou JSON) '''
+          def PreFormatacao(Formato):
               DadosLimpos = []
            
               for TextoCru in RelatorioCru:
@@ -24,25 +23,26 @@ class Formatar():
                   for Indice in range(len(self.Substituir)): 
                       TextoCru = TextoCru.replace(self.Substituir[Indice],self.Substitutos[Indice])
                   DadosLimpos.append(TextoCru)
-                  return(DadosLimpos)
-            
-           def HTML(self):
+              
+              DadosFormatados = Formato(DadosLimpos) ''' Passa o dados formatados para tal formato '''
+          
+          @PreFormatacao 
+          def HTML(self,DadosLimpos):
             ''' Pega o relatório e o formate em HTML. '''
-               NovosDadosEmHTML = []
-               for Dados in DadosLimpos:
-                   Dados = str(Dados)
-                   ''' Gera o relatório em HTML ''' 
-                   if Tipo is Tipos[0]:
-                      for Campos in self.CamposEmNegrito: ''' transforma tais campos em negrito, utilizando o tag <b> '''
-                          if Dados.split(":")[0] == Campos:
-                             Dados = Dados.replace(Campos,"<b>"+Campos+"</b>") 
+              NovosDadosEmHTML = []
+              for Dados in DadosLimpos:
+                  Dados = str(Dados)
+                  ''' Gera o relatório em HTML ''' 
+                  if Tipo is Tipos[0]:
+                     for Campos in self.CamposEmNegrito: ''' transforma tais campos em negrito, utilizando o tag <b> '''
+                         if Dados.split(":")[0] == Campos:
+                            Dados = Dados.replace(Campos,"<b>"+Campos+"</b>") 
 
-                          if Dados.split(":")[0] == "Incidente" or Dados.split(":")[0] == "Solicitação": '''  transforma em hyperlink o campo referente ao chamado '''
-                             NumeroDoChamado = Dados.split(":")[1]
-                             NumeroDoChamado = NumeroDoChamado.replace(" ","")
-                             Dados = "<a href="+UrlDoServiceDesk+NumeroDoChamado+" target=\"_blank\">"+Dados+"</a>" 
+                         if Dados.split(":")[0] == "Incidente" or Dados.split(":")[0] == "Solicitação": '''  transforma em hyperlink o campo referente ao chamado '''
+                            NumeroDoChamado = Dados.split(":")[1]
+                            NumeroDoChamado = NumeroDoChamado.replace(" ","")
+                            Dados = "<a href="+UrlDoServiceDesk+NumeroDoChamado+" target=\"_blank\">"+Dados+"</a>" 
 
-                             NovosDadosEmHTML.append(Dados)
-                    return(NovosDadosEmHTML)
-            def JSON(self):
-            ''' 
+                            NovosDadosEmHTML.append(Dados)
+               return(NovosDadosEmHTML)
+          return(DadosFormatados)
